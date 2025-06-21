2713f2e1a5470fefb87e5b64e049bd4b8325417f
@Aasav_0608 <aasav.pandya@lucentinnovation.com>
2025-06-22 00:08:49 +0530

    add:User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,70 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: function (value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      required: true,
+      validate: function (value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo url: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: function (value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password: " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
